a7419144ebd59e0c506445770947a4edba93a580
tkantae <109052365+tkantae@users.noreply.github.com>
2023-12-18 19:44:49 +0700

    ADD

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <body>
+        <h1>This is PHP  <?php  echo 'Hello World!'?> </h1>
+        <div>
+            <p>1</p>
+            <?php
+            if(false) {
+                # die;
+                //
+                /*
+                */
+            
+            ?>
+            <p>2</p>
+            <?php
+            }
+            ?>
+            <p>3</p>
+        </div>
+    </body>
+</html>
